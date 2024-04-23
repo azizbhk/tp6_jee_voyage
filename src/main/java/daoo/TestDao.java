8aed7441c985fa7b3351321498c113001593fa9c
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-04-23 19:44:51 +0100

    Update TestDao.java

--- src/main/java/daoo/TestDao.java
+++ src/main/java/daoo/TestDao.java
@@ -6,7 +6,7 @@ import metier.entities.voyage;
 public class TestDao {
 public static void main(String[] args) {
 	voyageDaolmpl pdao= new ();
-voyageDepa= pdao.save(new voyage("voyageinformatique ", 3, null));
+voyageDepa= pdao.save(new voyage("voyage"));
 System.out.println(VOY);
 List<voyage> VOY =pdao.voyageParMC("voyage");
 for (voyaged : VOY)
