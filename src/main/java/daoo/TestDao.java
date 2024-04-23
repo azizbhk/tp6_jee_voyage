4bac224c63a93f3ce7b6092c8c14bbdedf37db30
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-04-23 19:33:24 +0100

    Update TestDao.java

--- src/main/java/daoo/TestDao.java
+++ src/main/java/daoo/TestDao.java
@@ -7,9 +7,9 @@ public class TestDao {
 public static void main(String[] args) {
 	voyageDaolmpl pdao= new ();
 voyageDepa= pdao.save(new voyage("voyageinformatique ", 3, null));
-System.out.println(Depa);
-List<voyage> Depar =pdao.voyageParMC("voyage");
-for (voyaged : Depar)
+System.out.println(VOY);
+List<voyage> VOY =pdao.voyageParMC("voyage");
+for (voyaged : VOY)
 System.out.println(d);
 }
 }
