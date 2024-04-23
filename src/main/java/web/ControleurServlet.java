9d58c990b8dd6d5522749911c51c7f9a7eaa4fa0
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-04-23 19:40:47 +0100

    Update ControleurServlet.java

--- src/main/java/web/ControleurServlet.java
+++ src/main/java/web/ControleurServlet.java
@@ -11,23 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.catalina.connector.Response;
 
-import daoo.DepartementDaolmpl;
+import daoo.voyageDaolmpl;
 import daoo.typevoyageDaoImpl;
-import daoo.IDepartementDAO;
+import daoo.IvoyageDAO;
 import daoo.ItypevoyageDao;
-import daoo.IDepartementDAO;
+import daoo.IvoyageDAO;
 import metier.entities.typevoyage;
-import metier.entities.Departement;
+import metier.entities.voyage;
 import metier.entities.typevoyage;
 @WebServlet (name="cs",urlPatterns= {"/controleur","*.do"})
 public class ControleurServlet extends HttpServlet {
-IDepartementDAO metier;
+IvoyageDAO metier;
 ItypevoyageDao metierCol ; 
 @Override
 public void init() throws ServletException {
 	System.out.println("*************************************************************************************************************");
 
-metier = new DepartementDaolmpl();
+metier = new voyageDaolmpl();
 metierCol = new typevoyageDaoImpl() ; 
 }
 @Override
@@ -37,23 +37,23 @@ protected void doGet(HttpServletRequest request,
 		String path=request.getServletPath();
 		 if (metier == null) {
 		        // Initialize metier if it's null
-		        metier = new DepartementDaolmpl();
+		        metier = new voyageDaolmpl();
 		    }
 
 		if (path.equals("/index.do"))
 		{
 
-		request.getRequestDispatcher("departement.jsp").forward(request,response);
+		request.getRequestDispatcher("voyage.jsp").forward(request,response);
 		}
 		else if (path.equals("/chercher.do"))
 		{
 			String motCle=request.getParameter("motCle");
 			VoyageModele model= new VoyageModele();
 			model.setMotCle(motCle);
-			List<Departement> prods = metier.DepartementParMC(motCle);
-			model.setDepartement(prods);
+			List<voyage> prods = metier.voyageParMC(motCle);
+			model.setvoyage(prods);
 			request.setAttribute("model", model);
-			request.getRequestDispatcher("departement.jsp").forward(request,response);
+			request.getRequestDispatcher("voyage.jsp").forward(request,response);
 		}
 		else if (path.equals("/create.do") )
 		{
@@ -64,7 +64,7 @@ protected void doGet(HttpServletRequest request,
 			TypeVoyageModel model= new TypeVoyageModel();
 			model.settypevoyages(col);
 			request.setAttribute("colModel", model);
-			request.getRequestDispatcher("saisieDepartement.jsp").forward(request,response);		}
+			request.getRequestDispatcher("saisievoyage.jsp").forward(request,response);		}
 		
 		
 		
@@ -75,14 +75,14 @@ protected void doGet(HttpServletRequest request,
 		else if (path.equals("/save.do") &&
 		request.getMethod().equals("POST"))
 		{
-		 String NOM_departement=request.getParameter("NOM_departement");
+		 String NOM_voyage=request.getParameter("NOM_voyage");
 		int Nb_employee = Integer.parseInt(request.getParameter("Nb_employee"));
 		Long idCol=Long.parseLong(request.getParameter("typevoyage"));
 
 		typevoyage col = metierCol.gettypevoyage(idCol);
 
-		Departement p = metier.save(new Departement(NOM_departement,Nb_employee , col));
-		request.setAttribute("departement", p);
+		voyage p = metier.save(new voyage(NOM_voyage,Nb_employee , col));
+		request.setAttribute("voyage", p);
 		request.getRequestDispatcher("confirmation.jsp").forward(request,response);
 		}
 		
@@ -90,35 +90,35 @@ protected void doGet(HttpServletRequest request,
 		else if (path.equals("/supprimer.do"))
 		{
 		 Long id= Long.parseLong(request.getParameter("id"));
-		 metier.deleteDepartement(id);
+		 metier.deletevoyage(id);
 		 response.sendRedirect("chercher.do?motCle=");
 		}
 		else if (path.equals("/editer.do") )
 		{
 			
 		Long id= Long.parseLong(request.getParameter("id"));
-		 Departement p = metier.getDepartement(id);
-		 request.setAttribute("departement", p);
+		 voyage p = metier.getvoyage(id);
+		 request.setAttribute("voyage", p);
 		 
 		 List<typevoyage> col = metierCol.getAlltypevoyage();
 		 TypeVoyageModel model= new TypeVoyageModel();
 		 model.settypevoyages(col);
 		 request.setAttribute("colModel", model);
-		request.getRequestDispatcher("editerDepartement.jsp").forward(request,response);		}
+		request.getRequestDispatcher("editervoyage.jsp").forward(request,response);		}
 		else if (path.equals("/update.do") )
 		{
 		Long id = Long.parseLong(request.getParameter("id"));
-		String NOM_departement=request.getParameter("NOM_departement");
+		String NOM_voyage=request.getParameter("NOM_voyage");
 		int Nb_employee =Integer.parseInt(request.getParameter("Nb_employee"));
 		Long typevoyageId=Long.parseLong(request.getParameter("typevoyage"));
 
-		Departement p = new Departement();
-		p.setID_departement(id);
-		p.setNOM_departement(NOM_departement);
+		voyage p = new voyage();
+		p.setID_voyage(id);
+		p.setNOM_voyage(NOM_voyage);
 		p.setNb_employee(Nb_employee); 
 		typevoyage col = metierCol.gettypevoyage(typevoyageId);
 		p.settypevoyage(col);
-		metier.updateDepartement(p);
+		metier.updatevoyage(p);
 		response.sendRedirect("chercher.do?motCle=");
 
 		}
