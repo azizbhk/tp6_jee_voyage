593b6d2db5a23dfc604ad041dd3cd06bdc24b858
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-04-23 19:32:37 +0100

    Update TestDao.java

--- src/main/java/daoo/TestDao.java
+++ src/main/java/daoo/TestDao.java
@@ -2,13 +2,13 @@ package daoo;
 
 
 import java.util.List;
-import metier.entities.Departement;
+import metier.entities.voyage;
 public class TestDao {
 public static void main(String[] args) {
-	DepartementDaolmpl pdao= new DepartementDaolmpl();
-voyageDepa= pdao.save(new Departement("voyageinformatique ", 3, null));
+	voyageDaolmpl pdao= new ();
+voyageDepa= pdao.save(new voyage("voyageinformatique ", 3, null));
 System.out.println(Depa);
-List<Departement> Depar =pdao.DepartementParMC("departement");
+List<voyage> Depar =pdao.voyageParMC("voyage");
 for (voyaged : Depar)
 System.out.println(d);
 }
